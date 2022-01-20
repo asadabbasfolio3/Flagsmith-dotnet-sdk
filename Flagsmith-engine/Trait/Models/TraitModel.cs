f0d3ede6e258f366cb051259c9ddc3756331a603
FOLIO3\asadabbas <asadabbas@folio3.com>
2022-01-20 11:50:19 +0500

    set model attributes for json map

--- Flagsmith-engine/Trait/Models/TraitModel.cs
+++ Flagsmith-engine/Trait/Models/TraitModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Flagsmith_engine.Trait.Models
+﻿using Newtonsoft.Json;
+
+namespace Flagsmith_engine.Trait.Models
 {
     public class TraitModel
     {
+        [JsonProperty("trait_key")]
         public string TraitKey { get; set; }
+        [JsonProperty("trait_value")]
         public object TraitValue { get; set;}
     }
 }
