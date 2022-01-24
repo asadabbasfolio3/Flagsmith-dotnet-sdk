c9b052942d06647b4baf144688b189fbf672c1a0
Muhammad Rafay <muhammadrafay151@gmail.com>
2022-01-24 17:50:30 +0500

    Environment test implemented

---
+++ Flagsmith-engine/Feature/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flagsmith_engine.Feature
+{
+    public class Constants
+    {
+        public const string STANDARD = "STANDARD";
+        public const string MULTIVARIATE = "MULTIVARIATE";
+    }
+}
