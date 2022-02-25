caaafdb69ba160deca446118159d6e450a35d940
Muhammad Rafay <muhammadrafay151@gmail.com>
2022-02-25 11:34:43 +0500

    changes made as per PR

--- Flagsmith-dotnetSDK/Flag.cs
+++ Flagsmith-dotnetSDK/Flag.cs
@@ -46,23 +46,23 @@ namespace Flagsmith
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             JObject jo = JObject.Load(reader);
-            object targetObj = Activator.CreateInstance(objectType);
-            foreach (PropertyInfo prop in objectType.GetProperties().Where(p => p.CanRead && p.CanWrite))
+            object targetObj = Activator.CreateInstance(objectType);//create instance of the type which use this attribute i.e  [JsonConverter(typeof(FlagJsonConverter))]
+            foreach (PropertyInfo prop in objectType.GetProperties().Where(p => p.CanRead && p.CanWrite)) //using refeection retrieve all the properties from the type.
             {
-                JsonPropertyAttribute att = prop.GetCustomAttributes(true)
+                JsonPropertyAttribute att = prop.GetCustomAttributes(true)// from each property retrieve json property attribute i.e  [JsonProperty("id")]
                                                 .OfType<JsonPropertyAttribute>()
                                                 .FirstOrDefault();
 
-                string jsonPath = (att != null ? att.PropertyName : prop.Name);
-                JToken token = jo.SelectToken(jsonPath);
-
+                string jsonPath = (att != null ? att.PropertyName : prop.Name);//if there is json proprty attrbitue defined then the relative value used otherwise the original property name will be used.
+                JToken token = jo.SelectToken(jsonPath); //SelectToken is a method on JToken and takes a string path to a child token. i.e feature.name
+                // convert the token to object and set the proprty.
                 if (token != null && token.Type != JTokenType.Null)
                 {
                     object value = token.ToObject(prop.PropertyType, serializer);
                     prop.SetValue(targetObj, value, null);
                 }
             }
-
+            //return the fully mapped object.
             return targetObj;
         }
 
@@ -73,7 +73,7 @@ namespace Flagsmith
         }
 
         public override bool CanWrite
-        { 
+        {
             get { return false; }
         }
 
